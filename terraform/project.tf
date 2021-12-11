88b53576083235eec17ecb8c53d0b34e2e11b18b
Andreas Wagner <mailbox.wagner@gmail.com>
2021-12-11 10:06:59 +0100

    Add do project

---
+++ terraform/project.tf
@@ -0,0 +1,6 @@
+/*resource "digitalocean_project" "do-challenge" {
+  name        = "do-challenge"
+  environment = "Development"
+  resources   = [digitalocean_kubernetes_cluster.cluster.urn]
+}
+*/
